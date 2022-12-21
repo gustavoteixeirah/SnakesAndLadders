5a66897da69840e39d334d2f658d58854f74ee74
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-12-21 03:13:35 -0300

    Changed Board methods to look better

--- src/main/java/dev/gustavoteixeira/server/Board.java
+++ src/main/java/dev/gustavoteixeira/server/Board.java
@@ -14,51 +14,45 @@ public class Board {
 
     public Board() {
         resetPositions();
-        this.snakes = new HashMap<>();
-        snakes.put(36, 6);
-        snakes.put(32, 10);
-        snakes.put(62, 18);
-        snakes.put(88, 24);
-        snakes.put(48, 26);
-        snakes.put(95, 56);
-        snakes.put(97, 78);
-
-        this.ladders = new HashMap<>();
-        ladders.put(1, 38);
-        ladders.put(4, 14);
-        ladders.put(8, 30);
-        ladders.put(21, 42);
-        ladders.put(50, 67);
-        ladders.put(71, 92);
-        ladders.put(80, 99);
+        snakes = getSnakes();
+        ladders = getLadders();
     }
 
-
     public void movePlayers() {
-        int positionsToMove = ThreadLocalRandom.current().nextInt(1, 6);
-        System.out.println("Client rolled: " + positionsToMove);
-        this.client += positionsToMove;
-
+        moveClient();
         if (this.client >= 100) {
             System.out.println("Client wins the game!");
             resetPositions();
         }
+        checkForSnakesAndLadders();
 
-        ladders.computeIfPresent(this.client, (k, v) -> this.client = ladders.get(k));
-        snakes.computeIfPresent(this.client, (k, v) -> this.client = snakes.get(k));
-
-
-        positionsToMove = ThreadLocalRandom.current().nextInt(1, 6);
-        System.out.println("Server rolled: " + positionsToMove);
-        this.server += positionsToMove;
-
+        moveServer();
         if (this.server >= 100) {
             System.out.println("Server wins the game!");
             resetPositions();
         }
+        checkForSnakesAndLadders();
+    }
 
-        ladders.computeIfPresent(this.server, (k, v) -> this.server = ladders.get(k));
-        snakes.computeIfPresent(this.server, (k, v) -> this.server = snakes.get(k));
+    private void moveServer() {
+        int positionsToMove = getPositionsToMove();
+        System.out.println("Server rolled: " + positionsToMove);
+        this.server += positionsToMove;
+    }
+
+    private void moveClient() {
+        int positionsToMove = getPositionsToMove();
+        System.out.println("Client rolled: " + positionsToMove);
+        this.client += positionsToMove;
+    }
+
+    private static int getPositionsToMove() {
+        return ThreadLocalRandom.current().nextInt(1, 7);
+    }
+
+    private void checkForSnakesAndLadders() {
+        ladders.computeIfPresent(this.client, (k, v) -> this.client = ladders.get(k));
+        snakes.computeIfPresent(this.client, (k, v) -> this.client = snakes.get(k));
     }
 
     private void resetPositions() {
@@ -73,4 +67,33 @@ public class Board {
     public int getServer() {
         return server;
     }
+
+
+    private static Map<Integer, Integer> getSnakes() {
+        final Map<Integer, Integer> snakes;
+        snakes = new HashMap<>();
+        snakes.put(36, 6);
+        snakes.put(32, 10);
+        snakes.put(62, 18);
+        snakes.put(88, 24);
+        snakes.put(48, 26);
+        snakes.put(95, 56);
+        snakes.put(97, 78);
+        return snakes;
+    }
+
+    private static Map<Integer, Integer> getLadders() {
+        final Map<Integer, Integer> ladders;
+        ladders = new HashMap<>();
+        ladders.put(1, 38);
+        ladders.put(4, 14);
+        ladders.put(8, 30);
+        ladders.put(21, 42);
+        ladders.put(50, 67);
+        ladders.put(71, 92);
+        ladders.put(80, 99);
+        return ladders;
+    }
+
+
 }
